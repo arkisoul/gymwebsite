d82ebe603545d701b6c8feefcdcbea6aa0563cee
Arpit <arpit_jain@live.com>
2017-12-01 15:42:23 +0530

    Project resource files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+/* Start: On Page Load */
+	var myVar;
+				
+	function myFunction(){
+		myVar = setTimeout(showPage, 1000);
+	}
+				
+	function showPage(){
+		document.getElementById("loader").style.display = "none";
+		document.getElementById("myDiv").style.display = "block";	
+	}
+	
+/* End On Page Load */
+
+/* Start Form Validation */
+
+
+
+/* End Form Validation */
